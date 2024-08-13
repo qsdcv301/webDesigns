1586b45156c5e55b326a737b8f831e1c46b30855
qsdcv301 <qsdcv301@naver.com>
2024-08-13 10:48:13 +0900

    haeundae 공동작업 완료

--- haeundae/js/script.js
+++ haeundae/js/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
-    $(function () {
-        $('ul.gnb>li').hover(function () {
-            $(this).find('.lnb').stop().slideToggle();
-        });
+    $('ul.gnb>li').hover(function () {
+        $(this).find('.lnb').stop().slideToggle();
     });
+
+    $(".tab-nav>a").click(function (e) {
+        e.preventDefault();
+        $(".tab-nav>a").removeClass("active");
+        $(".tab-content>div").removeClass("act");
+        $(this).addClass("active");
+        const act = $(this).data("act");
+        $(act).addClass("act");
+    });
+
+    $(".pop").click(function (e) {
+        e.preventDefault();
+        $(".popup").fadeIn();
+    });
+
+    $(".close").click(function (e) {
+        e.preventDefault();
+        $(".popup").fadeOut();
+    });
+
+    setInterval(fadeInout, 3000);
 });
+
+function fadeInout() {
+    $(".hero img:eq(0)").fadeOut(500).next().fadeIn(500).end().appendTo(".hero");
+}
