823270bcca35e5ba554260582bc147982e10527f
qsdcv301 <qsdcv301@naver.com>
2024-08-20 11:09:18 +0900

    슬라이드 부분 공동작업

--- kimchi/js/script.js
+++ kimchi/js/script.js
@@ -5,4 +5,16 @@ $(document).ready(function () {
         $(this).find(".sub-nav").stop().slideToggle();
     });
 
+    function sliderImg() {
+        $(".hero-in").animate({
+            "left": "-100%"
+        }, 300, function () {
+            $(".hero-in img:eq(0)").clone().appendTo(".hero-in");
+            $(".hero-in img:eq(0)").remove();
+            $(".hero-in").css("left", 0);
+        }
+        );
+    }
+
+    setInterval(sliderImg, 3000);
 });
